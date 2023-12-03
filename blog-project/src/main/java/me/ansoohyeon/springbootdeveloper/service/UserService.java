b982d70029b77baf7e552dc075b4d46e671cc704
ansoohyeon <dkstngus0615@gmail.com>
2023-12-03 21:00:12 +0900

    Ch 7-8

---
+++ blog-project/src/main/java/me/ansoohyeon/springbootdeveloper/service/UserService.java
@@ -0,0 +1,25 @@
+package me.ansoohyeon.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.ansoohyeon.springbootdeveloper.domain.User;
+import me.ansoohyeon.springbootdeveloper.dto.AddUserRequest;
+import me.ansoohyeon.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
+}
